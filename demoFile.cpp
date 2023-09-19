6c6ab03fcb6e0d93d692b8fde4541a5fcc522248
hamza iqbal <i200856@nu.edu.pk>
2023-09-19 21:42:32 +0500

    9 commit

--- demoFile.cpp
+++ demoFile.cpp
@@ -6,6 +6,7 @@ int main()
     cout << "Hello World";
     cout << "devops Lab 1";
     cout << "devops Lab 2";
+    cout << "check for merge";
     cout << "devops Lab 3";
     cout << "devops Lab 4-merged commit";
     return 0;
